b1af57d259c51f2c2424e9ec7e9c24f7b49111d8
TheByronHimes <TheByronHimes@gmail.com>
2023-05-30 08:03:40 +0000

    Update name in setup.cfg to match dir
    Include version and desc in root __init__

--- src/__init__.py
+++ src/__init__.py
@@ -13,3 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""Upload benchmarking script for datahubs to evaluate infrastructure and S3 compatibility"""
+
+__version__ = "0.1.0"
